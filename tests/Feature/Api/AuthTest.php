a2e4bb90abd9d07be4a18a50412f58804cf98a70
aupous <tuan.phamvan.m@gmail.com>
2019-07-15 02:10:20 +0700

    feat: add more test case for auth test

--- tests/Feature/Api/AuthTest.php
+++ tests/Feature/Api/AuthTest.php
@@ -30,6 +30,38 @@ class AuthTest extends TestCase
         ]);
     }
 
+    // Dang ki user moi, truyen thieu 1 trong cac truong username | name | password | email
+    public function test_cannot_register_new_user_if_missing_field_email() {
+        $user = factory(User::class)->make();
+
+        $response = $this->json('POST', '/api/auth/register', [
+            'user' => [
+                'name' => $user->name,
+                'username' => $user->username,
+                'password' => 'password',
+            ],
+        ]);
+
+        $response->assertStatus(422);
+    }
+
+    // Dang ki user moi, truyen du cac truong, user name da ton tai
+    public function test_cannot_register_new_user_if_username_exists() {
+        $user = factory(User::class)->create();
+        $newUser = factory(User::class)->make();
+
+        $response = $this->json('POST', '/api/auth/register', [
+            'user' => [
+                'email' => $newUser->email,
+                'name' => $newUser->name,
+                'username' => $user->username,
+                'password' => 'password',
+            ],
+        ]);
+
+        $response->assertStatus(422);
+    }
+
     public function test_can_login()
     {
         $user = factory(User::class)->create();
@@ -47,6 +79,21 @@ class AuthTest extends TestCase
             'name' => $user->name,
             'username' => $user->username,
         ]);
+    }
+
+    // Dang nhap sai mat khau hoac username
+    public function test_cannot_login_if_wrong_password()
+    {
+        $user = factory(User::class)->create();
+
+        $response = $this->json('POST', '/api/auth/login', [
+            'user' => [
+                'username' => $user->username,
+                'password' => '123',
+            ],
+        ]);
+
+        $response->assertStatus(500);
     }
 
     public function test_can_get_user_profile()
