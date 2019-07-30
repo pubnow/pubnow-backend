bdbc5a9c8de8f470e2e7485aabb01227587a30f7
aupous <tuan.phamvan.m@gmail.com>
2019-07-30 22:46:44 +0700

    feat: follow organization:

---
+++ database/migrations/2019_07_30_134045_create_user_follow_organizations_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserFollowOrganizationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_follow_organizations', function (Blueprint $table) {
+            $table->uuid('id')->default(DB::raw('gen_random_uuid()'))->primary('id');
+
+            $table->uuid('user_id');
+            $table->foreign('user_id')->references('id')->on('users')
+                ->onUpdate('cascade')->onDelete('cascade');
+
+            $table->uuid('organization_id');
+            $table->foreign('organization_id')->references('id')->on('organizations')
+                ->onUpdate('cascade')->onDelete('cascade');
+
+            $table->unique(['user_id', 'organization_id']);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_follow_organizations');
+    }
+}
