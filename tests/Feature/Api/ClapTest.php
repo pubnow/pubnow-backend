33eea4ad1c097effc37fa19c0749efd85b9947ad
aupous <tuan.phamvan.m@gmail.com>
2019-08-01 23:05:36 +0700

    feat: add test case list

--- tests/Feature/Api/ClapTest.php
+++ tests/Feature/Api/ClapTest.php
@@ -19,4 +19,14 @@ class ClapTest extends TestCase
 
         $response->assertStatus(200);
     }
+    // --- Clap
+    // Test clap a exists article, logged in -> 201
+    // Test clap a exists article, logged in, clapped -> 200
+    // Test clap a exists article, not logged in -> 401
+    // Test clap a not exists article, logged in -> 404
+
+    // --- Unclap
+    // Test unclap a exists article, clapped -> 204
+    // Test unclap a exists article, not clapped -> 404
+    // Test
 }
